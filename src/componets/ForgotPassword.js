4ba1e615e7c5a5a78d448aed3035737c460f4724
ibrahimkonlan <haruna.konlan@amalitech.org>
2022-04-20 20:18:05 +0000

    forgotPassword

--- src/componets/ForgotPassword.js
+++ src/componets/ForgotPassword.js
@@ -1,2 +1,8 @@
 import React , { useRef } from 'react'
 import { useUserContext } from '../context/userContext';
+
+const ForgotPassword = () => {
+
+    const emailRef = useRef();
+  const { forgotPassword } = useUserContext();
+
