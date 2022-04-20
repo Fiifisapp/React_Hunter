3c1683f13927d78f52b67e9f60e732e03fc6adfd
emf21 <emefa.ameku@amalitech.org>
2022-04-20 20:03:10 +0000

    SignIn function

--- src/componets/Login.js
+++ src/componets/Login.js
@@ -1,3 +1,10 @@
 import React, { useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { useUserContext } from "../context/userContext";
+
+const Signin = () => {
+  const emailRef = useRef();
+  const psdRef = useRef();
+  const { signInUser } = useUserContext();
+  const history = useHistory();
+};
