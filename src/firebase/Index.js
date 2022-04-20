d5cd599f3c2035da6a955082b9a9544c74f05883
__francis <francis.coffie@amalitech.org>
2022-04-20 21:25:00 +0000

    storage bucket

--- src/firebase/Index.js
+++ src/firebase/Index.js
@@ -6,3 +6,5 @@ const firebaseConfig = {
     apiKey: "AIzaSyCzGKXxJ8IUpYRa5xqQkWZOmUChSAlVKMo",
     authDomain: "fir-auth-react-e8399.firebaseapp.com",
   projectId: "fir-auth-react-e8399",
+  storageBucket: "fir-auth-react-e8399.appspot.com",
+  messagingSenderId: "962605851630",
