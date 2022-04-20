c060c45fd7e498c5167bbab6a67e32b3047153cf
Fiifisapp <91861643+Fiifisapp@users.noreply.github.com>
2022-04-20 16:56:59 +0000

    imports

--- src/componets/LandingPage.js
+++ src/componets/LandingPage.js
@@ -1,4 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import mobileLogo from '../../assets/landing_page/phone/logo.svg'
+import mobileCaptured from '../../assets/landing_page/phone/captured_man.png';
+import mobileAnalysis from '../../assets/landing_page/phone/analysis.svg';
+import mobileDetails from '../../assets/landing_page/phone/details.svg';
+import mobileArrow from '../../assets/landing_page/phone/bi_arrow.png';
+import tabletAnalysis from '../../assets/landing_page/tablet/analysis.svg';
+import desktopCaptured from '../../assets/landing_page/desktop/captured_man.png';
+import desktopDetails from '../../assets/landing_page/desktop/details.svg';
 
 const LandingPage = () => {
   return (
