c9aaf1603688de6a9db4727525adcb9709040001
emf21 <emefa.ameku@amalitech.org>
2022-04-20 21:22:21 +0000

    Sign up form

--- src/componets/SignUp.js
+++ src/componets/SignUp.js
@@ -14,4 +14,17 @@ const Signup = () => {
     const password = psdRef.current.value;
     if (email && password && name) registerUser(email, password, name);
   };
+
+  return (
+    <div className="form">
+      <h2> New User</h2>
+      <form onSubmit={onSubmit}>
+        <input placeholder="Email" type="email" ref={emailRef} />
+        <input placeholder="Name" type="name" ref={nameRef} />
+        <input placeholder="Password" type="password" ref={psdRef} />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
+export default Signup;
