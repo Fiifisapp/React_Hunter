41f606a677c9a9e81b0266dde81acdfe4647e14e
emf21 <emefa.ameku@amalitech.org>
2022-04-20 20:26:07 +0000

    login form

--- src/componets/Login.js
+++ src/componets/Login.js
@@ -23,4 +23,17 @@ const Signin = () => {
     //     emailRef.current.value = "";
     //   });
   };
+
+  return (
+    <div className="form">
+      <h2> Login </h2>
+      <form onSubmit={onSubmit}>
+        <input placeholder="Email" type="email" ref={emailRef} />
+        <input placeholder="Password" type="password" ref={psdRef} />
+        <button type="submit">Sign In</button>
+        <p onClick={forgotPasswordHandler}>Forgot Password?</p>
+      </form>
+    </div>
+  );
 };
+export default Signin;
