b1ece67d0f984f77797e1f41ba9f73b1a7e36633
Fiifisapp <91861643+Fiifisapp@users.noreply.github.com>
2022-04-20 20:30:21 +0000

    imported pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import LandingPage from './componets/LandingPage';
 import Auth from './componets/Auth';
+import ForgotPassword from './componets/ForgotPassword';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/auth" element={<Auth/>}/>
+        <Route path="/forgot" element={<ForgotPassword/>}/>
       </Routes>
     </Router>
   );
