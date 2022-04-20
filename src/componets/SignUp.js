61c17e7b0b7f0684ef2d08993a0cf8335d1dd53d
emf21 <emefa.ameku@amalitech.org>
2022-04-20 21:19:37 +0000

    sign up function

--- src/componets/SignUp.js
+++ src/componets/SignUp.js
@@ -1,2 +1,9 @@
 import React, { useRef } from "react";
 import { useUserContext } from "../context/userContext";
+
+const Signup = () => {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const psdRef = useRef();
+  const { registerUser } = useUserContext();
+};
