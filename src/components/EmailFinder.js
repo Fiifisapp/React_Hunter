95d61037af6f3a77e993677d3a24d4b1d4929c99
__francis <francis.coffie@amalitech.org>
2022-04-24 11:37:15 +0000

    first css

--- src/components/EmailFinder.js
+++ src/components/EmailFinder.js
@@ -6,35 +6,37 @@ import "./emailfinder.styles.css";
 
 const EmailFinder = () => {
 
-  const [firstname, setFirstname] =useState();
+  const [firstname, setFirstname] = useState();
   const [lastname, setLastname] = useState();
-  const [domain, setDomain] =useState();
+  const [domain, setDomain] = useState();
 
   const handleFind = () => {
     emailFinder()
   }
 
   const emailFinder = async () => {
-    try{
+    try {
       const data = await axios.get(`https://api.hunter.io/v2/email-finder?domain=${domain}&first_name=${firstname}&last_name=${lastname}&api_key=ff389661c63ab90742cfe61e1f82343073b8893e`)
 
-      if(data){
+      if (data) {
         console.log(data)
       }
-    } catch (err){
+    } catch (err) {
       console.log(err);
     }
   }
 
   return (
     <div>
-        <Navigation/>
-         <input type="text" placeholder='first name' onChange={e => setFirstname(e.target.value)}/>
-         <input type="text" placeholder='last name' onChange={e => setLastname(e.target.value)}/>
-         <input type="text" placeholder='domain' onChange={e => setDomain(e.target.value)}/>
-         <button onClick={handleFind}>
-           find
-         </button>
+      <Navigation />
+      <form>
+        <input type="text" className="email-style" placeholder='first name' onChange={e => setFirstname(e.target.value)} />
+        <input type="text" className="email-style" placeholder='last name' onChange={e => setLastname(e.target.value)} />
+        <input type="text" className="email-style" placeholder='domain' onChange={e => setDomain(e.target.value)} />
+        <button className ="email-button" onClick={handleFind}>
+          find
+        </button>
+      </form>
     </div>
   )
 }
